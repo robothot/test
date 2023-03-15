90d8a84acaa06a500b39724d4e9b5447ed47fb2b
robothot <854363956@qq.com>
2023-03-15 09:20:13 +0800

    Rust Publish Static Files

---
+++ 449.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkms_admin_template=self.webpackChunkms_admin_template||[]).push([[449],{45449:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var a=n(35944);const s=function(){return(0,a.tZ)(a.HY,{children:"这个是首页信息"})}}}]);
